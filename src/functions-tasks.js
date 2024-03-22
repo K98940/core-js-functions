864a8c0dfecbccd1f502c71f575a0ecff8881e71
k98940 <pv1xxb@yandex.ru>
2024-03-22 10:32:37 +0600

    feat: getPowerFunction, memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -70,8 +70,8 @@ function getArgumentsCount(...funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (x) => x ** exponent;
 }
 
 /**
@@ -105,8 +105,9 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const x = func();
+  return () => x;
 }
 
 /**
